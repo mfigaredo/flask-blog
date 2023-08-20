2f83a14df232b78505b9873b08d5204991a2fe12
Miguel Figaredo <mfigaredo@gmail.com>
2023-08-20 14:31:04 -0600

    Sixth commit

--- run_prod.py
+++ run_prod.py
@@ -2,9 +2,12 @@ import os
 # from dotenv import load_dotenv
 # load_dotenv()
 
-from flaskblog import create_app
+from flaskblog import create_app, db
 
 app = create_app()
+
+with app.app_context():
+    db.create_all()
 
 if __name__ == '__main__':
     app.run(debug=False, host="0.0.0.0", port=os.getenv('PORT', default=5000))
